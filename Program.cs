fb79b0a7b38730f18ebb75645e22f11d6db1318f
Matheus Bronzel <matheusbronzel@hotmail.com>
2026-01-18 12:18:22 -0300

    Finalizado o desafio de POO!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "IMEI123456", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\nSmartphone iPhone:");
+Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 13", imei: "IMEI987654", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
